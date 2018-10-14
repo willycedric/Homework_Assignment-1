d1ed93b1945e70310d090376ea1c9a699b3ff466
Willy DONGMO <willydongmo@MacBook-de-Willy.local>
2018-10-14 14:40:57 +0200

    assignment done

--- app/index.js
+++ app/index.js
@@ -8,16 +8,10 @@
  const server= require('./server')
  //Import the configuration file
  const config = require('./config')
- const app = require('express')()
 //instantiate the http server
  const httpServer = http.createServer((req, res)=>{
     server(req, res)
  })
-
- app.post('/',(req,res)=>{
-     
- })
-
  //start the http server
  httpServer.listen(config.httpPort, ()=>{
      console.log(` Server is running on the ${config.envName} environnment and  listening on the port ${config.httpPort}.`)
